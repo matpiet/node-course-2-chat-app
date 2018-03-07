53a0df95e62727d79aa426bc973da4fb237fc0aa
Pietsch <matt.pietsch@lynchburgva.gov>
2018-03-07 16:39:57 -0500

    commit socket.io changes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const publicPath = path.join(__dirname, '..', 'public');
 port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>{
+  console.log('New User Connected');
+}); //registers an event listener
 
 console.log(__dirname + '\\..\\public');
 console.log(publicPath);
 
-app.listen(port, () => console.log('Example app listening on port 3000!'));
+server.listen(port, () => console.log('Example app listening on port 3000!'));
