33f67ba604f7fd49af7abb315a336ca222203132
Pietsch <matt.pietsch@lynchburgva.gov>
2018-03-07 15:51:43 -0500

    commit changes in package

--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 
 const publicPath = path.join(__dirname, '..', 'public');
+port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 
 console.log(__dirname + '\\..\\public');
 console.log(publicPath);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(port, () => console.log('Example app listening on port 3000!'));
