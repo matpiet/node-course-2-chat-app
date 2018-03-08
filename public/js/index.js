69dba134e191db8b85b141c7b9398f452212d489
Matt P <matpiet@yahoo.com>
2018-03-07 22:30:00 -0500

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 
 socket.on('connect', function(){
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    to: 'Jim Acosta',
-    text: 'Whats going on with all the Russia stuff?'
-  });
 });
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -13,14 +13,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
   console.log('New User Connected');
 
-  socket.emit('newMessage', {
-    from: 'Stan Lee',
-    text: 'Do you draw in the comic books good coloring?'
-  });
-
   socket.on('createMessage', (message) => {
     console.log(JSON.stringify(message, undefined, 3));
-    console.log('help!!');
+
+    io.emit('newMessage', {
+      from: message.to,
+      text: message.text
+    });
   });
 }); //registers an event listener
 
