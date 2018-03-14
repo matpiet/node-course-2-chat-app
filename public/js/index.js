be7b01f9e3d299ef7209913965f92c6a8c9baaf2
Matt P <matpiet@yahoo.com>
2018-03-13 22:02:12 -0400

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -10,5 +10,28 @@ socket.on('disconnect', function(){
 });
 
 socket.on('newMessage', function(message) {
-    console.log(JSON.stringify(message, undefined, 3));
+    //console.log(JSON.stringify(message, undefined, 3));
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+
+socket.emit('createMessage', {
+  from: "Frank",
+  text: "Frank is happy with life!"
+}, function (message){
+  console.log(message);
+});
+
+$('#message-form').on('submit', function(e) {
+  //alert('here');
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'Matt',
+    text: $('[name=message]').val()
+  }, function(message){
+  console.log(message);
+});
 });
