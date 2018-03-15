7bf5adcb6b710c42b7b1fb938a24464f6d928972
Matt P <matpiet@yahoo.com>
2018-03-14 22:56:05 -0400

    added some styles

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -12,6 +12,18 @@ describe('generateMessage', () => {
     expect(message.text).toBe('Some Message');
 
     expect(message).toMatchObject({from, text});
-
   });
-})
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object.', () => {
+    let from = 'Testman';
+    let lat = 51.5007;
+    let long = 0.1246;
+    let url = 'https://www.google.com/maps?q=51.5007,0.1246';
+    var message = generateLocationMessage(from, lat, long);
+    expect(message.url).toBe(url);
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({from, url});
+  });
+});
